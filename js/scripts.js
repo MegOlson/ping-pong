df16ee2bec3b97b14320dec82e8d633f93cb15b7
mo <meganannetteolson@yahoo.com>
2017-08-18 10:11:42 -0700

    Save input number with all preceding numbers as array

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,34 @@
 // Business End Logic
 
 function errorCheck(number) {
-  if (!number || number !== 0-9) {
+  if (!number || number === /[^0-9]+/g) {
     return "error: no input or NaN";
   }
 }
-
-
-
+function inputConversion(number) {
+  var error = errorCheck(number);
+  if (error) {
+    return error;
+  } else {
+    var numberArray = [];
+    for (var index = number; index > 0; index--) {
+      numberArray.push(index);
+    }
+    return numberArray;
+  }
+}
 
 // User Interface Logic
 $(document).ready(function() {
   $("#number").submit(function(event) {
     event.preventDefault();
-    var number = $("input#number-input").val();
-    var numberOutput = errorCheck(number);
+    var number = parseInt($("input#number-input").val());
+    var numberOutput = inputConversion(number);
     console.log(numberOutput);
+    // if (errorCheck === "error: no input or NaN") {
+    //   $(".alert").show();
+    // } else {
+    //   $("#result").text(inputConversion(number));
+    // }
   });
 });
